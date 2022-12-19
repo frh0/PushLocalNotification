258ffbfe929db97f1af2b704d553f4312fef9c18
frh <frh.shaalan@gmail.com>
2022-12-19 14:12:28 +0300

    Initial Commit

---
+++ push/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  push
+//
+//  Created by frh alshaalan on 25/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ push/pushApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushApp.swift
+//  push
+//
+//  Created by frh alshaalan on 25/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct pushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
